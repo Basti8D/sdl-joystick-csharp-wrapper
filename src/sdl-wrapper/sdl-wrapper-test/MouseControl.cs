131a553bc757ebde15635bd69dc038ed35cf5760
Basti8D <bastian.lehmann@quickline.ch>
2026-09-12 17:49:50 +0200

    add mouse controll class

---
+++ src/sdl-wrapper/sdl-wrapper-test/MouseControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WindowsInput;
+using WindowsInput.Events;
+
+namespace sdl_wrapper_test
+{
+    public class MouseControl
+    {
+        public MouseControl() { }
+
+        public static async Task LeftClick()
+        {
+            await Simulate.Events().Click(ButtonCode.Left).Invoke();
+        }
+
+        public static async Task LeftClick(int ms)
+        {
+            await Simulate.Events().Hold(ButtonCode.Left).Wait(ms).Release(ButtonCode.Left).Invoke();
+        }
+
+        public static async Task RightClick()
+        {
+            await Simulate.Events().Click(ButtonCode.Right).Invoke();
+        }
+
+        public static async Task RightClick(int ms)
+        {
+            await Simulate.Events().Hold(ButtonCode.Right).Wait(ms).Release(ButtonCode.Right).Invoke();
+        }
+        public static async Task RelativeMove (int x, int y)
+        {
+            await Simulate.Events().MoveBy(x, y).Invoke();
+        }
+
+        public static async Task AbsoluteMove(int x, int y)
+        {
+            await Simulate.Events().MoveTo(x, y).Invoke();
+        }
+    }
+}
